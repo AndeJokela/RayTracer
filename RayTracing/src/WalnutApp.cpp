1d676ed5792b23b99612951e93336d8342e9a897
Antti Jokela <antti.s.jokela@tuni.fi>
2023-05-15 06:47:51 +0300

    Camera system

--- RayTracing/src/WalnutApp.cpp
+++ RayTracing/src/WalnutApp.cpp
@@ -5,12 +5,21 @@
 #include <Walnut/Timer.h>
 
 #include "Renderer.h"
+#include "Camera.h"
 
 using namespace Walnut;
 
 class ExampleLayer : public Walnut::Layer
 {
 public:
+	ExampleLayer()
+		: m_Camera(45.0f, 0.1f, 100.0f) {}
+
+	virtual void OnUpdate(float ts) override
+	{
+		m_Camera.OnUpdate(ts);
+	}
+
 	virtual void OnUIRender() override
 	{
 		ImGui::Begin("Settings");
@@ -43,13 +52,15 @@ public:
 		Timer timer;
 		
 		m_Renderer.OnResize(m_ViewportW, m_ViewportH);
-		m_Renderer.Render();
+		m_Camera.OnResize(m_ViewportW, m_ViewportH);
+		m_Renderer.Render(m_Camera);
 
 		m_LastRenderTime = timer.ElapsedMillis();
 	}
 
 private:
 	Renderer m_Renderer;
+	Camera m_Camera;
 	uint32_t m_ViewportW = 0;
 	uint32_t m_ViewportH = 0;
 	float m_LastRenderTime = 0.0f;
